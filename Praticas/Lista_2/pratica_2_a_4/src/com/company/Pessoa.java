1928fc3ed5ed667dfda1dd64b3415bd6c3343744
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2021-05-04 15:00:28 -0300

    Add pratica 2 a 4

---
+++ Praticas/Lista_2/pratica_2_a_4/src/com/company/Pessoa.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Pessoa {
+    String nome;
+    Endereco endereco;
+    String numero;
+
+    public Endereco consultaEndereco(){
+        return endereco;
+    }
+
+    public String consultaNome() {
+        return nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
